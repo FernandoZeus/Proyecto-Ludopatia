ef7002e940508e303d561e14edc2b099e8e4b08f
Andres Mamani <63963121+AndresMamani@users.noreply.github.com>
2020-04-19 18:01:23 +0200

    Update GiTprueba.java

--- Git/src/GiTprueba.java
+++ Git/src/GiTprueba.java
@@ -3,7 +3,7 @@ public class GiTprueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("hola, se ha matao paco");
+System.out.print("hola, se ha matao paco. RIP");
 	}
 
 }

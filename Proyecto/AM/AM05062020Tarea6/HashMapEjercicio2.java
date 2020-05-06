eb29542b7e7c1ddcc91dd5adb2a1e29a8877aa7e
xd <xd@DESKTOP-93SUVK3>
2020-05-06 17:24:39 +0200

    Tareas Andres

---
+++ Proyecto/AM/AM05062020Tarea6/HashMapEjercicio2.java
@@ -0,0 +1,25 @@
+package AM05062020Tarea6;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import Recurso.Persona; 
+
+public class HashMapEjercicio2 {
+
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	//Est� mal 
+	public static void imprimePorNombre(HashMap<String, ArrayList<Persona>> listaImprimir) {
+		
+		for(char l = 'A'; l <='Z'; l++) {
+			System.out.println("\nCon la letra: " + (l + "" + "\n"));
+			for (int i = 0; i < listaImprimir.size(); i++) {
+				System.out.println(listaImprimir);
+			}
+        }
+	}
+}

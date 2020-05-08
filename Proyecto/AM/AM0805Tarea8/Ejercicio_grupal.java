fa74cfdd774a47fe6cd0f3de0f908c7cba8d1e31
Carlo <Carlo@DESKTOP-QOH6AJH>
2020-05-08 16:36:21 +0200

    Completo

---
+++ Proyecto/AM/AM0805Tarea8/Ejercicio_grupal.java
@@ -0,0 +1,29 @@
+package AM0805Tarea8;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Ejercicio_grupal {
+
+	public static void main(String[] args) throws IOException {
+		
+		String leido;
+		FileReader leerFichero = new FileReader("D:\\Users\\Carlo\\Desktop\\notas.txt");
+		BufferedReader br = new BufferedReader(leerFichero);
+		FileWriter notas2 = new FileWriter("D:\\Users\\Carlo\\Desktop\\notas_.txt");
+		
+		try {
+			while((leido = br.readLine())!=null) {
+	            for (int i = 0; i < leido.length(); i++) {
+	            	leido = leido.replaceAll("sobresaliente", "aprobado");
+				}
+	            notas2.write(leido + "\n");
+	      	}
+			notas2.flush();
+		}catch(Exception e) {
+			System.out.println("Algo ha salido mal.");
+		}
+	}
+}
